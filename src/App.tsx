04e0e8d05086daee0eb27c5aec6bd999d8b724bb
Rupesh Sharma <rupeshsharma21@Rupeshs-MacBook-Pro.local>
2026-02-24 16:06:07 +0530

    Initial commit: Vite + React + TS + Tailwind v4 setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-4xl font-bold text-blue-600 hover:text-red-500 transition-colors">
+      Hello Tailwind v4 + React + TypeScript! 🚀
+    </div>
+  )
+}
+
+export default App
